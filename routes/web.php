1dc5e4611cb79de845b095557575616ca7f7e622
atik <atik.parallax@gmail.com>
2018-01-08 16:52:55 +0600

    Authentication Done

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+
+
+Route::get('/registration', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('registration/post', 'Auth\RegisterController@register');
+
+/*Auth::routes();*/
+
+Route::get('/home', 'HomeController@index')->name('home');
